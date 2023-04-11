7e57b936d29e79237ba1c61d3c40b94142c0d411
Елена Ведерникова <el1157567v@yandex.ru>
2023-04-11 16:42:13 +0300

    Added model Comment

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,81 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class User(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField(
+        verbose_name='Текст отзыва',
+        max_length=1000
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField(
+        verbose_name='Оценка',
+        validators=[
+            MinValueValidator(1, 'Выберите оценку от 1 до 10'),
+            MaxValueValidator(10, 'Выберите оценку от 1 до 10')
+        ]
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ['pub_date']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_review'
+            ),
+        ]
+
+    def __str__(self) -> str:
+        return self.text[:15]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        verbose_name='Отзыв',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField(
+        verbose_name='Текст комментария',
+        max_length=500
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ['pub_date']
+
+    def __str__(self) -> str:
+        return self.text[:15]
