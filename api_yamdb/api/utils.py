56afb163f05985cc7fd7bfb5c502244d9e052636
Daniil Fedin <nniiyu@yandex.ru>
2023-04-19 14:28:58 +0300

    Добавлена отправка кода подтверждения на email

---
+++ api_yamdb/api/utils.py
@@ -0,0 +1,12 @@
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
+
+
+def send_confim_code(user):
+    code = default_token_generator.make_token(user)
+    send_mail(
+        'Confim code',
+        f'username: {user.username}, confirmation_code: {code}',
+        'admin@yamdb.com',
+        [user.email],
+    )
