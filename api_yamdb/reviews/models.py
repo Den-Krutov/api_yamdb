6e9bc72642cc7bf938ef5ec19b2afabbb185a576
Andrej Troshin <darkusss13@ya.ru>
2023-05-03 09:39:13 +0300

    Исправление warning

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -70,6 +70,9 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']
+
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
@@ -77,6 +80,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
 
 
 class Title(models.Model):
@@ -93,6 +99,9 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
 
 
 class GenreTitle(models.Model):
