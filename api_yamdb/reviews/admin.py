05a90c3688b109d7ce1326c52afa96e1fec449e2
Елена Ведерникова <el1157567v@yandex.ru>
2023-05-03 17:29:23 +0300

    дополнен admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Review, Comment, Title, Category, Genre
+from .models import User, Review, Comment, Title, Category, Genre, GenreTitle
 
 
 @admin.register(User)
@@ -13,3 +13,4 @@ admin.site.register(Comment)
 admin.site.register(Title)
 admin.site.register(Category)
 admin.site.register(Genre)
+admin.site.register(GenreTitle)
