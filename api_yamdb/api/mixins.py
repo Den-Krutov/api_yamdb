1747a39f1b1f89328920acad850630fdf60b4d26
Andrej Troshin <darkusss13@ya.ru>
2023-05-10 07:17:03 +0300

    Исправлены ошибки после ревью

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,8 +1,16 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import (mixins, viewsets)
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import filters
+from api.permissions import AdminOrReadOnly
 
 
 class CreateListDestroyViewSet(mixins.CreateModelMixin,
                                mixins.ListModelMixin,
                                mixins.DestroyModelMixin,
                                viewsets.GenericViewSet):
-    pass
+    permission_classes = [AdminOrReadOnly]
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    pagination_class = PageNumberPagination
+    search_fields = ['name']
+    lookup_field = 'slug'
